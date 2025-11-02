7fd86140319c035a98ff46a2899882415e709b73
euvalennn <97848208+euvalennn@users.noreply.github.com>
2025-11-01 22:44:20 -0300

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false,
   },
+
+  adapter: vercel(),
 });
